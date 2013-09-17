c0fbc1d0b75e18d80c4d131fa03546038c7f80de
Aliou Diallo <aliou.diallo@me.com>
2013-09-17 10:13:57 +0200

    Edit chart option for better readability.

--- public/js/music.js
+++ public/js/music.js
@@ -1,4 +1,8 @@
 var ctx = document.getElementById("myChart").getContext("2d");
 var myNewChart = new Chart(ctx).Line(music.music, {
   bezierCurve: false,
+  scaleOverride : true,
+  scaleSteps : 15,
+  scaleStepWidth : 10,
+  scaleStartValue : 0,
 });
