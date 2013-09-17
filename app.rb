899fd367b00eadeafd22b242923e4b2d2b0f8184
Aliou Diallo <aliou.diallo@me.com>
2013-09-17 09:41:50 +0200

    Load Data from redis and send it in the view.

--- app.rb
+++ app.rb
@@ -1,6 +1,15 @@
 require 'sinatra'
+require 'date'
+require 'json'
+require 'redis'
+
+REDIS_SERVER = ENV['REDISTOGO_URL'] || "redis://localhost:6379"
+uri = URI.parse(REDIS_SERVER)
+$redis = Redis.new(host: uri.host, port: uri.port, password: uri.password)
 
 class Music < Sinatra::Application
   get '/' do
+    @music = $redis['songs']
+    erb :index
   end
 end
