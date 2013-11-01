b7d7496190883000bc5ff696c8d5e73ba6dae45e
Aliou Diallo <code@aliou.me>
2013-11-01 12:03:31 +0100

    Limit use of global variables.

--- app.rb
+++ app.rb
@@ -4,9 +4,9 @@ require 'json'
 require 'redis'
 require 'lastfm'
 
-REDIS_SERVER = ENV['REDISTOGO_URL'] || "redis://localhost:6379"
-uri = URI.parse(REDIS_SERVER)
-$redis = Redis.new(host: uri.host, port: uri.port, password: uri.password)
+REDIS_SERVER = ENV['REDISTOGO_URL']
+uri = URI.parse REDIS_SERVER
+$redis = Redis.new host: uri.host, port: uri.port, password: uri.password
 
 $lastfm_client = Lastfm.new ENV['LASTFM_KEY'], ENV['LASTFM_SECRET']
 
@@ -14,9 +14,9 @@ class Music < Sinatra::Application
   get '/' do
     @song_count = $redis['song_count']
     @artist_count = $redis['artist_count']
-    @last_song = $lastfm_client.user.get_recent_tracks(:user => 'aliouftw',
+    last_song = $lastfm_client.user.get_recent_tracks(:user => 'aliouftw',
                                                        :limit => 1)
-    @last_song = @last_song.first unless @last_song.class != Array
+    @last_song = last_song.first unless last_song.class != Array
     @np_str = @last_song.has_key?("date") ? "Last played" : "Now Playing"
     erb :index
   end
